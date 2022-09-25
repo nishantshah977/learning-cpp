2dbb15ae9d62ae6165cf6974dc431991ca74735f
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-09-25 20:33:51 +0545

    Create second.cpp

---
+++ second.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+for(n=n;n=n;n--){
+        cout<<n<<"\n";
+              if(n%5==0){
+        	cout<<"Beep"<<"\n";
+        }  
+    }
+    
+    
+    return 0;
+}
