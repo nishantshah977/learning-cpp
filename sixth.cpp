1d5724da042887667e5bc0195e66ec8854e48add
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-10-03 19:34:35 +0545

    Create sixth.cpp

---
+++ sixth.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int recSum(int n) {
+    //complete the function
+    if(n<=1){
+        return 1;
+    }
+    return n + recSum(n-1);
+}
+
+int main() {
+    //getting input
+    int n;
+    cin >> n;
+    
+    //call the recursive function and print returned value
+    cout<<recSum(n);
+    
+    return 0;
+}
